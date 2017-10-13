e5c21fc9d8b040724401af35bc4ff07a6194a85a
Kroiririoroirkk <taoeric9@gmail.com>
2017-10-12 20:25:14 -0400

    Update demo.py
    Updated to PEP 8.

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@ from mathsolver.solver import *
 print('3x + 3', stringToAST('3x + 3'), sep='; ')
 print('boo(1)', stringToAST('boo(1)'), sep='; ')
 try:
-  print('3+', stringToAST('3+'), sep='; ')
+    print('3+', stringToAST('3+'), sep='; ')
 except ValueError as e:
-  print(str(e))
+    print(str(e))
 print('abcf + 8 + 10 + abcf', simplifyStr('abcf + 8 + 10 + abcf'), sep='; ')
 print('f((4+x^2)(x^2-7), 6)', expandStr('f((4+x^2)(x^2-7), 6)'), sep='; ')
 print('x^2+x=fx-3', *solve('x', expandStr('x^2+x=fx-3')), sep='; ')
