fb2506d84c75c513e0b4f07bd9e2adbe200515b0
Kroiririoroirkk <taoeric9@gmail.com>
2017-10-12 20:29:19 -0400

    Update demo.py
    Moved solveIO from solver.py to demo.py.

--- demo.py
+++ demo.py
@@ -10,4 +10,18 @@ print('abcf + 8 + 10 + abcf', simplifyStr('abcf + 8 + 10 + abcf'), sep='; ')
 print('f((4+x^2)(x^2-7), 6)', expandStr('f((4+x^2)(x^2-7), 6)'), sep='; ')
 print('x^2+x=fx-3', *solve('x', expandStr('x^2+x=fx-3')), sep='; ')
 print('j=5-j', *solve('j', expandStr('j=5-j')), sep='; ')
+
+
+def solveIO():
+    equationStr = input('Enter equation: ')
+    var = input('Enter variable to solve for: ')
+    solutions = solve(var[0], stringToAST(equationStr))
+    if isinstance(solutions, str):
+        print('Error:', solutions)
+    else:
+        print('Answer:',
+              ', '.join([var[0] + ' = ' + str(solution)
+                        for solution in solutions]))
+
+
 solveIO()
